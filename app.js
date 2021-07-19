a8429a18d018901774871f758a179734c94f8560
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2021-07-19 15:15:17 +0530

    d

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-area");
+var outputDiv = document.querySelector("#txt-output");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+     console.log("error occured",error);
+     alert("Something went wrong")
+} 
+
+function dothis(){
+    var inputTxt = txtInput.value; 
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+            var translatedTxt = json.contents.translated;
+            outputDiv.innerText = translatedTxt;
+        })
+    .catch(errorHandler)       
+};
+btnTranslate.addEventListener("click", dothis);
